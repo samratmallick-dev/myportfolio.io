10367c9d7b67c3adeecfbc0b4da6200ae8f43cce
Samrat Mallick <samratmallick832@gmail.com>
2026-01-15 00:12:25 +0530

    update email config

--- backend/src/utilities/email/sendEmail.js
+++ backend/src/utilities/email/sendEmail.js
@@ -8,7 +8,7 @@ const createTransporter = () => {
             secure: false,
             auth: {
                   user: process.env.EMAIL_USER,
-                  pass: process.env.EMAIL_PASS,
+                  pass: process.env.EMAIL_PASS.replace(/\s/g, ""),
             },
       });
 };
